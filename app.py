e11cc63ccaec87a3f3c43c198c98a2a27fc1141d
debugtony <96636403+debugtony@users.noreply.github.com>
2023-10-18 14:22:40 +0000

    made dynamic pages from database

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template
 
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -18,6 +18,13 @@ def list_jobs():
   job_list = [dict(job) for job in jobs]
   return jsonify(job_list)
 
+
+@app.route("/job/<int:id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not Found", 404
+    return render_template('job_page.html', job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -18,3 +18,25 @@ def load_jobs_from_db():
     jobs = result.fetchall()
     return jobs
 
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+      result = conn.execute(
+          text("SELECT * FROM jobs WHERE id = :val").bindparams(val=id)
+      )
+      rows = result.all()
+
+      if len(rows) == 0:
+          return None
+      else:
+          # Convert the first row (which is a tuple) to a dictionary
+          job_data = {}
+          first_row = rows[0]  # Get the first row
+          for i, key in enumerate(result.keys()):
+              job_data[key] = first_row[i]
+          return job_data
+
+
+
+
+
